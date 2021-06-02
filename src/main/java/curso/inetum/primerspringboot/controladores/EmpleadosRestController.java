a860d3aa0e03792646b1637f1ca6852f06cde9ca
Tu Nombre <you@example.com>
2021-06-02 12:31:25 +0200

    up

--- src/main/java/curso/inetum/primerspringboot/controladores/EmpleadosRestController.java
+++ src/main/java/curso/inetum/primerspringboot/controladores/EmpleadosRestController.java
@@ -1,5 +1,6 @@
 package curso.inetum.primerspringboot.controladores;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -11,6 +12,7 @@ import lombok.Data;
 @Data
 public class EmpleadosRestController {
 	
+	@Autowired
 	private EmpleadoCrudRepository repository;
 	
 	@GetMapping("empleados")
