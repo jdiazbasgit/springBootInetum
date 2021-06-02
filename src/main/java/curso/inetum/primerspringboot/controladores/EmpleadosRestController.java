6978d1b252c9b6c1548402fd0c5f2148af226282
Tu Nombre <you@example.com>
2021-06-02 12:15:22 +0200

    rest empleados

---
+++ src/main/java/curso/inetum/primerspringboot/controladores/EmpleadosRestController.java
@@ -0,0 +1,23 @@
+package curso.inetum.primerspringboot.controladores;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import curso.inetum.primerspringboot.entidades.Empleado;
+import curso.inetum.primerspringboot.repositorios.EmpleadoCrudRepository;
+import lombok.Data;
+
+@RestController
+@Data
+public class EmpleadosRestController {
+	
+	private EmpleadoCrudRepository repository;
+	
+	@GetMapping("empleados")
+	public Iterable<Empleado> empleados(){
+		
+		return getRepository().findAll();
+		
+	}
+
+}
--- src/main/java/curso/inetum/primerspringboot/repositorios/EmpleadoCrudRepository.java
+++ src/main/java/curso/inetum/primerspringboot/repositorios/EmpleadoCrudRepository.java
@@ -4,6 +4,7 @@ import org.springframework.data.repository.CrudRepository;
 
 import curso.inetum.primerspringboot.entidades.Empleado;
 
+
 public interface EmpleadoCrudRepository extends CrudRepository<Empleado, Integer> {
 
 }
